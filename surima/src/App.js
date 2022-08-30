5823460a1a90500f7888cd48917d30697f385c0d
Guille A <LGuille.2000@gmail.com>
2022-08-30 11:33:32 -0300

    desafioBoton

--- surima/src/App.js
+++ surima/src/App.js
@@ -1,4 +1,5 @@
 import './styles/app.scss';
+import {useState} from 'react';
 import Navbar from './components/Navbar/Navbar.jsx';
 import IconosFlotantes from "./components/IconosFlotantes/IconosFlotantes.jsx";
 import { Productos } from "./components/Productos/Productos";
@@ -9,12 +10,18 @@ import gohan from "./components/Productos/img/Gohan.JPG";
 import empanadas from "./components/Productos/img/Empanadas.JPG";
 import piezas from "./components/Productos/img/Piezas.JPG";
 
-const agregar = (acu) => {
-  console.log(acu)
-}
+
 
 
 function App() {
+  const [numeroProductos, setNumerosProductos] = useState(0);
+
+  const agregar = (acu) => {
+    setNumerosProductos(acu)
+  }
+
+  console.log("numeroProductos", numeroProductos)
+  
   return (
     <div className="App">
       <Contador stock={5} agregarProducto={agregar}/>
